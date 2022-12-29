e050b30a1e31503a4cf4028da7660a760cfc4023
juliosc1 <julio.siklander@gmail.com>
2022-12-29 18:16:05 +0100

    add task function

--- components/Todolist.tsx
+++ components/Todolist.tsx
@@ -1,22 +1,37 @@
 import React, { ChangeEvent, useState } from "react";
 
-type todolist = {
-  id: number;
-  task: string;
+type TodoListProps = {
+
+  taskName: string;
 };
 
-function Todolist(props: todolist) {
+function Todolist() {
   const [task, setTask] = useState<string>("");
+  const [todoList, setTodoList] = useState<TodoListProps[]>([]);
 
   const handleTask = (event: ChangeEvent<HTMLInputElement>): void => {
     setTask(event.target.value);
   };
 
+  const addTask = (): void => {
+    const newTask = {taskName: task}
+    setTodoList([...todoList, newTask])
+    setTask("");
+  }
+
   return (
     <div className="flex flex-col justify-center items-center">
       <h1>My TodoList</h1>
-      <input type="text" value={task} onChange={handleTask} placeholder="Enter your task..." className="border" />
-      <button>Add Task</button>
+      <input type="text" value={task} name='task' onChange={handleTask} placeholder="Enter your task..." className="border" />
+      <button onClick={addTask}>Add Task</button>
+
+      <div>
+        {todoList.map((value) => {
+          return (
+            <h1>{value.taskName}</h1>
+          )
+        })}
+      </div>
     </div>
   );
 }
